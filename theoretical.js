64277e2820b444edfbe736bc92feb02cc046d9ea
Gunish <gunishsharma20@gmail.com>
2023-06-01 15:52:07 -0600

    works for 3x3 matrix

--- theoretical.js
+++ theoretical.js
@@ -10,7 +10,10 @@
 //     [8, 9, 4],
 //     [7, 6, 5]]
 //  matrix(4)
-//     [[1,   2,  3, 4], [12, 13, 14, 5],[11, 16, 15, 6], [10,  9,  8, 7]]
+//      [[1,   2,  3, 4],
+//      [12, 13, 14, 5],
+//      [11, 16, 15, 6], 
+//      [10,  9,  8, 7]]
 
 function spiral(N){
     const spiral = []
@@ -35,11 +38,34 @@ function spiral(N){
     console.log('after right is done spiral is: ', spiral)
     console.log(`total nums ${totalNums}`)
     //down
-    console.log(spiral[spiral.length-1])
-    console.log('after down is done, spiral is: ', spiral)
+    for(let i = 1; i<N;i++){
+        spiral[i].push(totalNums[i-1])
+    }
+    console.log('after down is done spiral is: ', spiral)
+    for(let i=0; i<=(N-2);i++){
+        totalNums.shift()
+    }
     console.log(`total nums ${totalNums}`)
     //left
-    //up
+    for(let i=0;i<N-1;i++){
+        
+        spiral[N-1].unshift(totalNums[i])
+    }
+    console.log('after left is done spiral is: ', spiral)
+    for(let i=0; i<=(N-2);i++){
+        totalNums.shift()
+    }
+    console.log(`total nums ${totalNums}`)
+    //up or right
+    for(let i=N-2;i>-1;i--){
+        
+        spiral[N-2].unshift(totalNums[i])
+    }
+
+    console.log(spiral[0])
+    console.log(spiral[1])
+    console.log(spiral[2])
+
 }
 
 spiral(3)
