9408c3ea6f3b06d039ff7869cd6987176eab5795
Gunish <gunishsharma20@gmail.com>
2023-06-02 02:27:55 -0600

    written done

--- theoretical.js
+++ theoretical.js
@@ -1,71 +1,71 @@
-// --- Directions
-// Write a function that accepts an integer N
-// and returns a NxN spiral matrix.
-// --- Examples
-//   matrix(2)
-//     [[undefined, undefined],
-//     [undefined, undefined]]
-//   matrix(3)
-//     [[1, 2, 3],
-//     [8, 9, 4],
-//     [7, 6, 5]]
-//  matrix(4)
-//      [[1,   2,  3, 4],
-//      [12, 13, 14, 5],
-//      [11, 16, 15, 6], 
-//      [10,  9,  8, 7]]
+/*
+MATRIX TESTING
+*/
+// exampleMat = [[0,0,0],
+//               [0,0,0],
+//               [0,0,0]]
+
+// exampleMat[0][1] =1
+// console.log(exampleMat[0])
+// console.log(exampleMat[1])
+// console.log(exampleMat[2])
 
 function spiral(N){
-    const spiral = []
-    const totalNums = []
-    for (let i = 1; i<=N; i++){
-        spiral.push([])
+    let counter =1
+    let startRow = 0
+    let endRow = N-1
+    let startCol = 0
+    let endCol = N-1
+    //Create Matrix
+    const mat = []
+    for(let i=0;i<N;i++){
+        mat.push([])
     }
-    console.log('init ', spiral)
-    for(let i=1; i<= (N**2);i++){
-        totalNums.push(i)
+    //top
+    for(let i = startRow; i<=endRow; i++){
+        console.log(`i : ${i}`)
+        mat[startRow][i] = counter
+        counter++
     }
-    console.log(`total nums ${totalNums}`)
-    //right
-    for(let i= 0;i<=(N-1);i++){
-        spiral[N-N].push(totalNums[i])
-        
-    }
-    for(let i=0; i<=(N-1);i++){
-        totalNums.shift()
-    }
-
-    console.log('after right is done spiral is: ', spiral)
-    console.log(`total nums ${totalNums}`)
+    startRow++
     //down
-    for(let i = 1; i<N;i++){
-        spiral[i].push(totalNums[i-1])
+    
+    for(let i = startRow;i<N; i++){
+        mat[i].push(counter)
+        counter++
     }
-    console.log('after down is done spiral is: ', spiral)
-    for(let i=0; i<=(N-2);i++){
-        totalNums.shift()
-    }
-    console.log(`total nums ${totalNums}`)
+    endCol--
     //left
-    for(let i=0;i<N-1;i++){
-        
-        spiral[N-1].unshift(totalNums[i])
-    }
-    console.log('after left is done spiral is: ', spiral)
-    for(let i=0; i<=(N-2);i++){
-        totalNums.shift()
-    }
-    console.log(`total nums ${totalNums}`)
-    //up or right
-    for(let i=N-2;i>-1;i--){
-        
-        spiral[N-2].unshift(totalNums[i])
-    }
+    console.log('Endrow : ',endRow,mat[3])
+    console.log('beg counter: ',counter)
 
-    console.log(spiral[0])
-    console.log(spiral[1])
-    console.log(spiral[2])
+    for(let i= endRow;i>startCol;i--){
+        console.log('counter: ',counter)
+        mat[endRow].unshift(counter)
+        counter++
+    }
+    endRow--
+    //up
+    console.log('endRow ',endRow)
+    console.log('startRow ',startRow)
+    for(let i = endRow;i>startRow;i--){
 
+        for(let i = 0;i<1;i++){
+            mat[endRow].unshift(counter)
+            counter++
+        }
+    }
+    //mat[3].unshift('2222')
+    
+    
+    console.log(`start row: ${startRow}`)
+
+
+    console.log(mat[0])
+    console.log(mat[1])
+    console.log(mat[2])
+    console.log(mat[3])
+    return mat
 }
 
-spiral(3)
+spiral(4)
